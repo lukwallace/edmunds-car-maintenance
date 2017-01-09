478db61d4af22b6cf6d3b7a554319e4ae897f659
Wallace <wallace.s.luk@gmail.com>
2017-01-08 16:36:02 -0800

    Functioning API calls

---
+++ config.example.js
@@ -0,0 +1,2 @@
+// To use this application - fill in the value below and rename this file to: config.js
+var API_KEY = "{YOUR API KEY HERE}";
--- index.js
+++ index.js
@@ -4,22 +4,31 @@ var makeRow = function(name, classname, disabled) {
   var $row = $(html);
   $('table').append($row);
 
-  var options = ['Select ' + classname.charAt(0).toUpperCase() + classname.slice(1)]
-  addOption(classname, options);
+  var option = 'Select ' + classname.charAt(0).toUpperCase() + classname.slice(1);
+  addOption(classname, option);
 };
 
-var addOption = function(classname, options) {
+var addOption = function(classname, option, value) {
   var $select = $('.' + classname);
-  options.forEach(function(option) {
-    $select.append($('<option>' + option + '</option>'));
-  });
+  $select.append($('<option value=\"' + value + '\">' + option + '</option>'));
 };
 
 var init = function(year) {
+  // Create row entries for the selections
   makeRow('Year:', 'year');
   makeRow('Vehicle Make:', 'make', true);
   makeRow('Vehicle Model:', 'model', true);
   makeRow('Vehicle Trim:', 'trim', true);
   makeRow('Vehicle Engine:', 'engine', true);
   makeRow('Vehicle Transmission:', 'transmission', true);
+
+  // Add years options
+  for(var i = 1990; i <= year; i++) {
+    addOption('year', '' + i, '' + i);
+  }
+
+  // Make interactive.
+  $('.year').change(function() {
+    populateMake(this.value);
+  });
 }(new Date().getFullYear());
